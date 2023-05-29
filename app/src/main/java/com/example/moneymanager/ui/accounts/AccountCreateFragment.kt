756ad8e222bf94e6598476b4a7d74bc0c84f16f0
AnnaKachmasheva <annakachmasheva@gmail.com>
2023-05-29 16:37:35 +0200

    added crud account

--- app/src/main/java/com/example/moneymanager/ui/accounts/AccountCreateFragment.kt
+++ app/src/main/java/com/example/moneymanager/ui/accounts/AccountCreateFragment.kt
@@ -9,18 +9,17 @@ import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
-import androidx.navigation.fragment.navArgs
 import com.example.moneymanager.model.AccountModel
+import com.example.sp_v2.R
 import com.example.sp_v2.databinding.FragmentEditAccountBinding
 
 
-class AccountEditFragment : Fragment() {
+class AccountCreateFragment : Fragment() {
 
     private var _binding: FragmentEditAccountBinding? = null
     private val binding get() = _binding!!
 
     private lateinit var mAccountViewModel: AccountsViewModel
-    private val args by navArgs<AccountFragmentArgs>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -29,39 +28,29 @@ class AccountEditFragment : Fragment() {
     ): View {
         _binding = FragmentEditAccountBinding.inflate(inflater, container, false)
 
-        // if update
-        binding.nameText.setText(args.accountModel.name)
-        binding.amountText.setText(args.accountModel.amount.toString())
-        binding.switchPayment.isChecked = args.accountModel.isIncludeInTotalBalance
-
-        binding.editAccountButton.text = "Save"
-
         mAccountViewModel = ViewModelProvider(this)[AccountsViewModel::class.java]
         binding.editAccountButton.setOnClickListener {
-            updateDataToDatabase()
+            insertDataToDatabase()
         }
         return binding.root
     }
 
-
-    private fun updateDataToDatabase() {
+    private fun insertDataToDatabase() {
         val name = binding.nameText.text.toString().trim()
         val amount = binding.amountText.text.toString().trim()
         val isIncludeInTotalBalance = !binding.switchPayment.isChecked
 
         if (inputCheck(amount, name)) {
             val account = AccountModel(
-                id = args.accountModel.id,
+                id = 0,
                 name = name,
                 amount = amount.toDouble(),
                 isIncludeInTotalBalance = isIncludeInTotalBalance
             )
-            mAccountViewModel.updateAccount(account)
-            Toast.makeText(requireContext(), "Account successfully updated!", Toast.LENGTH_LONG)
+            mAccountViewModel.addAccount(account)
+            Toast.makeText(requireContext(), "Account successfully added!", Toast.LENGTH_LONG)
                 .show()
-            val action =
-                AccountEditFragmentDirections.actionAccountEditFragmentToAccountFragment2(account)
-            findNavController().navigate(action)
+            findNavController().navigate(R.id.action_createAccountFragment_to_nav_accounts)
         } else {
             Toast.makeText(requireContext(), "Please fill out all fields.", Toast.LENGTH_LONG)
                 .show()
@@ -73,6 +62,7 @@ class AccountEditFragment : Fragment() {
             amount
         )
     }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
